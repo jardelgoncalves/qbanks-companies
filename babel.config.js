6cae129ca012c82d31de014f734bfa85e4b456f7
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-05-29 01:15:53 -0300

    chore: add styled components and module resolver plugins

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'babel-plugin-styled-components',
+    [
+      'module-resolver',
+      {
+        cwd: ['packagejson'],
+        root: ['.'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
