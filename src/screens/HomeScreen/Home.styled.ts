0b9d7938ca11023ad780919ee8ff02b844b8f619
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-05-30 13:40:38 -0300

    feat: add search input and list companies

--- src/screens/HomeScreen/Home.styled.ts
+++ src/screens/HomeScreen/Home.styled.ts
@@ -1,5 +1,22 @@
-import styled from 'styled-components/native';
+import {Dimensions, FlatList} from 'react-native';
+import styled, {css} from 'styled-components/native';
+const {height} = Dimensions.get('window');
 
-export const Text = styled.Text`
-  color: #fff;
+export const Container = styled.SafeAreaView`
+  ${({theme}) => css`
+    height: ${height - 80}px;
+    background-color: ${theme.colors.grayscale[100]};
+  `};
+`;
+
+export const List = styled.FlatList`
+  padding: 32px 35px 32px;
+` as unknown as typeof FlatList;
+
+export const Spacing = styled.View`
+  height: 64px;
+`;
+
+export const HeaderContainer = styled.View`
+  padding: 32px 35px 16px;
 `;
