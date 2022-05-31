f749dc0acc2574c363ad2027f72ce5e418f9228f
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-05-31 02:01:45 -0300

    feat: add companyId as optional prop

--- src/hooks/use-find-company.ts
+++ src/hooks/use-find-company.ts
@@ -2,13 +2,19 @@ import {Company} from '@/interfaces/company';
 import {getById} from '@/services/company-service';
 import {useEffect, useState} from 'react';
 
-export const useFindCompany = (id: string) => {
+export const useFindCompany = (id?: string) => {
   const [company, setCompany] = useState<Company | null>(null);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const findById = (companyId: string) => {
+  const findById = (companyId?: string) => {
     setLoading(true);
+    if (!companyId) {
+      setCompany(null);
+      setLoading(false);
+      return;
+    }
+
     getById(companyId)
       .then(data => {
         setCompany(data);
